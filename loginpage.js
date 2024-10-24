bfb593b7b5ee02e1d8b7ae7eac3b987d5a6e12c7
DanLutz555 <138560265+DanLutz555@users.noreply.github.com>
2024-10-24 11:06:10 +0300

    Add files via upload

--- loginpage.js
+++ loginpage.js
@@ -1,4 +1,30 @@
 //הוסיפו סקריפט שיבדוק האם המייל תואם ל doctor@gmail.com
+  window.onload = () => {
+    if (localStorage.getItem("loggedIn") === "true") {
+      window.location.href = "logged.html"; // העברה אוטומטית לעמוד logged.html אם כבר מחובר
+    }
+  };
+
+  const checkIf = () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email !== "doctor@gmail.com") {
+      alert("Invalid email, please try again.");
+      return false;
+    } else if (password !== "medic123") {
+      alert("Incorrect password, please try again.");
+      return false;
+    } else {
+      // שמירת מצב התחברות ב-localStorage
+      localStorage.setItem("loggedIn", "true");
+      window.location.href = "logged.html"; // העברה לעמוד logged.html
+      return true;
+    }
+  };
+
+  const button = document.getElementById("button");
+  button.addEventListener("click", checkIf);
 //בדקו גם שהסיסמה תואמת ל medic123
 //אם כן השתמשו ב wep api של הדפדפן והעבירו את המשתמש למסך אחר, כל עמוד שתבנו בשם logged.html
 //שימו לב אם המשתמש כבר התחבר, בעמוד זה logged.html יהיה לו כפתור להתנתקות
